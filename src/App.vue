985c39697a4b90134f4d97ab26a258aa36b471a4
OYAMA <947544040@qq.com>
2025-11-12 21:06:44 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <h1>elTable大数据加载优化</h1>
+
+  <el-table v-my-scroll height="300" :data="tableData.slice(start,over)">
+    <el-table-column prop="date" label="Date"  />
+    <el-table-column prop="name" label="Name"  />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+</div>
+
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        tableData: []
+      }
+    },
+    mounted() {
+      setTimeout(()=> {
+        let arr = []
+        for (let i = 0; i < 100; i++) {
+          arr.push({
+            date: `date_${i+1}`,
+            name: `name_${i+1}`,
+            address: `address_${i+1}`,
+          })
+        }
+        this.tableData = arr
+      }, 1000)
+    }
+  }
+</script>
+
+<style>
+.tableStyle {
+  margin: 0 auto;
+}
+.el-table__row {
+  height: 40px;
+}
+.el-table__row {
+  padding: 0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+Vue.directive('my-scroll', {
+  bind(el, binding, vnode) {
+    // console.log('vnode', vnode);
+    // 获取实例
+    const self = vnode.context
+    // 获取目标元素
+    let target = el.querySelector('.el-table__body-wrapper')
+    target.addEventListener('scroll', () => {
+      // 防抖
+      setTimeout(() => {
+        const _table = target.querySelector('table')
+        _table.style.paddingTop = self.padding[0] + 'px'
+        _table.style.paddingBottom = self.padding[1] + 50 + 'px'
+        self.tableHeight = target.clientHeight
+        self.scrollTop = target.scrollTop
+      }, 200);
+    })
+  }
+})
+
+Vue.mixin({
+  data() {
+    return {
+      tableHeight: 300,
+      scrollTop: 0,
+    }
+  },
+  computed: {
+    start() {
+      return Math.max(this.scrollTop / 40 - 10, 0)
+    },
+    over() {
+      return Math.min((this.scrollTop + this.tableHeight) / 40 + 10, this.tableData.length)
+    },
+    padding() {
+      let paddingBottom = (this.tableData - this.over) * 40
+      let paddingTop = this.start * 40
+      return [paddingTop, paddingBottom]
+    }
+  }
+})
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
